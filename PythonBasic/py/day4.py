7118282312a83c8c23ba63993279df13901eb169
lucy <tmdlucy0214@gmail.com>
2021-01-05 18:29:05 +0900

    while for practice

--- PythonBasic/py/day4.py
+++ PythonBasic/py/day4.py
@@ -118,11 +118,24 @@
 
 #연습문제
 # 1. a 리스트에서 중복 숫자를 제거해 보자.
-# a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
-#
+a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+a2=[]
+for i in a:
+    if i not in a2:
+        a2.append(i)
+print(a2)
+
 # 2. while문을 사용해 1부터 1000까지의 자연수 중 3의 배수이면서 7의 배수인
 # 수의 합을 구해 보자.
-#
+num=0
+sum=0
+while num<1000:
+    num+=1
+    if num%3==0 and num%7==0:
+        sum+=num
+print(sum)
+
+
 # 3. while문을 사용하여 다음과 같이 별(*)을 표시하는 프로그램을 작성해 보자.
 # 1)
 # *
@@ -130,51 +143,135 @@
 # ***
 # ****
 # *****
-#
+
+1번
+x=0
+while x<5:
+    x+=1
+    print('*'*x)
+
+2번
+x=0
+y=0
+while x<5:
+    x+=1
+    y=0
+    while y<x:
+        y+=1
+        print("*",end='')
+    print()
+
+3번
+for i in range(5):
+    for j in range(i+1):
+        print('*', end = '')
+    print() #한 줄이 끝나면 새 줄로 바꿈
+
 # 2)
 #      *
 #     **
 #    ***
 #   ****
 #  *****
+1번
+y=5
+while y>0:
+    y-=1
+    print(" "*y+"*"*(5-y))
+
+for 루프
+for i in range(1,6):
+    for j in range(5-i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()
+
 # 3)adv
 #      *
 #     ***
 #    *****
 #   *******
 #  *********
+
 #
-#
+z=0
+while z<10:
+    z+=1
+    if z%2==0:continue
+    print(' '*((9-z)//2)+'*'*(z))
+
+for 루프
+for i in range(1,6):
+    for j in range(6-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    print()
 # 4.
 # for문을 사용해 1부터 100까지의 숫자를 출력해 보자.
-#
+for a in range(1,101):
+   print(a)
+
 # 4-1.(adv)
 # for문을 사용해 2부터 100까지의 숫자 중에서 소수를(prime number) 출력해 보자.
 # *소수란? 1과 자기 자신으로만 나누어 떨어지는 수(ex. 2, 3, 5, 7, 11, 13,...)
-#
-#
+
+for i in range(2,101):
+    boolean=True
+    for j in range(2,i):
+        if i%j==0:
+            boolean=False
+            break
+    if boolean:
+        print(i, end=' ')
+
+
 # 5.
 # A 학급에 총 10명의 학생이 있다. 이 학생들의 중간고사 점수는 다음과 같다.
-# [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+score=[70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
 # for문을 사용하여 A 학급의 평균 점수를 구해 보자.
-#
+sum=0
+for s in score:
+    sum+=s
+print(sum/len(score))
+
+
 # 6. 로또 당첨 번호 제작(adv)
 # *주의:중복된 수 나오면 안됨
 # 이번 주 로또 당첨 번호 :  3 7 13 22 25 29
-#
-# 7. 자판기(pro, 커피 한 잔에 300원이라 가정, 초기 커피는 10개)
-# 돈을 넣어 주세요: 500
-# 거스름돈 200를 주고 커피를 줍니다.
-# 돈을 넣어 주세요: 300
-# 커피를 줍니다.
-# 돈을 넣어 주세요: 100
-# 돈을 다시 돌려주고 커피를 주지 않습니다.
-# 남은 커피의 양은 8개입니다.
-# 돈을 넣어 주세요: 0
-# 종료합니다
 
+import random
 
+for i in range(1,7):
+    num=random.randint(1,46)
+    print(num,end=" ")
 
+7. 자판기(pro, 커피 한 잔에 300원이라 가정, 초기 커피는 10개)
+돈을 넣어 주세요: 500
+거스름돈 200를 주고 커피를 줍니다.
+돈을 넣어 주세요: 300
+커피를 줍니다.
+돈을 넣어 주세요: 100
+돈을 다시 돌려주고 커피를 주지 않습니다.
+남은 커피의 양은 8개입니다.
+돈을 넣어 주세요: 0
+종료합니다
+coffee = 10
+while True:
+    money = int(input("돈을 넣어 주세요: "))
+    if money == 300:
+        print("커피를 줍니다.")
+        coffee = coffee -1
+    elif money > 300:
+        print("거스름돈 %d를 주고 커피를 줍니다." % (money-300))
+        coffee = coffee -1
+    else:
+        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+        print("남은 커피의 양은 %d개 입니다." % coffee)
+    if not coffee:
+        print("종료합니다")
+        break
 
 
 
